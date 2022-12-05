72c68e7b723be61edd864d52327122d20d4c1558
Adam Salhi <adamtherookie@proton.me>
2022-12-05 15:32:40 +0100

    Register login and logout

--- website/urls.py
+++ website/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("register", views.register, name="register"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout")
 ]
--- website/views.py
+++ website/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import login, authenticate, logout
+from django.urls import reverse
+from django.db import IntegrityError
 
 from .models import User
 
@@ -13,9 +17,51 @@ def register(request):
         return render(request, "website/registerorlogin.html", {
             'register':1
         })
+    else:
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
 
-def login(request):
+        # Ensure password matches confirmation
+        if password != confirmation:
+            return render(request, "website/registerorlogin.html", {
+                'register':1,
+                'message': "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "website/registerorlogin.html", {
+                'register':1,
+                'message': "Username already taken."
+            })
+        
+        login(request, user)
+        return HttpResponseRedirect(reverse('index'))
+
+def login_view(request):
     if request.method == "GET":
         return render(request, "website/registerorlogin.html", {
             'register':0
         })
+    else:
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse('index'))
+        else:
+            return render(request, "website/registerorlogin.html", {
+                'register':0,
+                'message':'Invalid username and/or password.'
+            })
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
